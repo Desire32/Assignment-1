7df989f0a7e51cdd02a974f57f12ae39e20a7dec
Desire32 <58604764+Desire32@users.noreply.github.com>
2023-12-03 18:38:05 +0200

    JS files Assignment 1

---
+++ js/burger-menu.js
@@ -0,0 +1,16 @@
+let burgerOpen = document.querySelector(".burger-menu-button");
+let burgerMenu = document.querySelector(".burger-menu");
+
+let isBurgerOpen = false;
+
+burgerOpen.onclick = function () {
+    if (!isBurgerOpen) {
+        burgerMenu.style.display = "block";
+        isBurgerOpen = true;
+    }
+    else if (isBurgerOpen) {
+            burgerMenu.style.display = "none";
+            burgerOpen.style.backgroundPosition = "center, center left 50px";
+            isBurgerOpen = false;
+        }
+}
