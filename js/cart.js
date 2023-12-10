5550136266e110e136bb2e5daa2d53155ba6347e
Desire32 <58604764+Desire32@users.noreply.github.com>
2023-12-11 01:33:44 +0200

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,31 +1,44 @@
 function shoppingCart(object) {
 	const cartContainer = document.getElementById('cartContainer');
-    cartContainer.innerHTML = '';
-	
+	cartContainer.innerHTML = '';
+
 	const cart = JSON.parse(localStorage.getItem('cart')) || [];
-	
-	cart.forEach(item => {
 
-		const cartContainer = document.getElementById('cartContainer');
+	cart.forEach((item, index) => {
 
-		const cartItem = document.createElement('div');
-		cartItem.classList.add('itemCart');
+			const cartItem = document.createElement('div');
+			cartItem.classList.add('itemCart');
 
-		const cartName = document.createElement('h3');
-		cartName.textContent = item[0] + " - " + item[1];
- 
-		const photoItem = document.createElement('img');
-		photoItem.src = item[4];
+			const itemDetails = document.createElement('div'); 
+    itemDetails.classList.add('itemDetails');
 
-		const priceItem = document.createElement('div');
-		priceItem.classList.add('price')
-		priceItem.textContent = item[3];
+			const cartDescription = document.createElement('div');
+			cartDescription.classList.add('description');
+			cartDescription.textContent = item[1];
 
-		cartItem.appendChild(photoItem);
-		cartItem.appendChild(cartName);
-		cartItem.appendChild(priceItem);
-		cartContainer.appendChild(cartItem);
-});
+			const cartName = document.createElement('h3');
+			cartName.textContent = item[0];
+
+			const cartIndex = document.createElement('div');
+			cartIndex.classList.add('index');
+			cartIndex.textContent = `${index}`;
+
+			const photoItem = document.createElement('img');
+			photoItem.src = item[4];
+
+			const priceItem = document.createElement('div');
+			priceItem.classList.add('price')
+			priceItem.textContent = item[3];
+
+			itemDetails.appendChild(photoItem);
+    itemDetails.appendChild(cartDescription);
+
+			cartItem.appendChild(cartIndex);
+			cartItem.appendChild(itemDetails);
+			cartItem.appendChild(cartName);
+			cartItem.appendChild(priceItem);
+			cartContainer.appendChild(cartItem);
+	});
 }
 
 shoppingCart();
