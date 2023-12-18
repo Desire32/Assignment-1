6c3da01ba602d45238fd5ced2552a36765f9159c
Desire32 <58604764+Desire32@users.noreply.github.com>
2023-12-18 14:34:52 +0200

    Commiting final changes

--- js/htmlToJSFooter.js
+++ js/htmlToJSFooter.js
@@ -1,20 +1,20 @@
+// converting the code from "html" and importing it from a js file for convenience, so as not to copy it for each page
+
 function htmlToJSFooter(){
   const footerElement = document.getElementById('footer');
   const firstPart = `
                 <div class="left">
                     <h2>Links</h2>
-                    <p><a href="">Student's Union</a></p>
+                    <p><a href="https://www.uclansu.co.uk/">Student's Union</a></p>
+                    <p><a href="https://github.com/Desire32/Assignment-1">GitHub</a></p>
                 </div>
             `;
-
             const secondPart = `
                 <div class="middle">
                     <h2>Contact</h2>
-                    <p>Email: suinformation@uclan.ac.uk</p>
-                    <p>Phone: 01772 89 3000</p>
+                    <p>Email: nmarkov@uclan.ac.uk</p>
                 </div>
             `;
-
             const thirdPart = `
                 <div class="right">
                     <h2>Location</h2>
@@ -28,5 +28,5 @@ function htmlToJSFooter(){
 
             footerElement.innerHTML = firstPart + secondPart + thirdPart;
 }
-
+// initialisation of function
 htmlToJSFooter();
